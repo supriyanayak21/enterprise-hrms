a232e8ecbf17141aa7537e82b92d2e484e5abef8
Supriya Nayak <nayakjhunu40@gmail.com>
2026-07-15 12:56:57 +0530

    Setup Express backend project

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const app = require("./src/app");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("=================================");
+  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🌐 http://localhost:${PORT}`);
+  console.log("=================================");
+});
