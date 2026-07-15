51d1aca42777aff136978719283829c12e36ee97
Supriya Nayak <nayakjhunu40@gmail.com>
2026-07-15 20:45:57 +0530

    feat(backend): configure MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-const app = require("./src/app");
 require("dotenv").config();
+
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 const PORT = process.env.PORT || 5000;
 
+// Connect Database
+connectDB();
+
 app.listen(PORT, () => {
   console.log("=================================");
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on Port ${PORT}`);
   console.log(`🌐 http://localhost:${PORT}`);
   console.log("=================================");
 });
